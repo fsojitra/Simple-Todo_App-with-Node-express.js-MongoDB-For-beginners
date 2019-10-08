97cd7c67ad3a7d2baa827b8a29481e5264405e81
Foram Sojitra <foram.sojitra295@gmail.com>
2019-10-08 16:28:58 +0530

    change callback wth arrow function

--- server.js
+++ server.js
@@ -1,76 +1,70 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var path = require('path');	
-var todo = require('./model/todo.js');
+var bodyParser = require("body-parser");
+var path = require("path");
+var todo = require("./model/todo.js");
 //both index.js and things.js should be in same directory
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo', { useMongoClient: true });
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/todo", { useMongoClient: true });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res){
-	res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.post('/', function(req, res){
-	console.log(req.body);
-	var todoInfo = req.body;
-	if(!todoInfo.todo){
-		console.log("No Info");
-	}else{
-		var c;
-		Todo.findOne({},function(err,data){
+app.post("/", (req, res) => {
+  var todoInfo = req.body;
+  if (!todoInfo.todo) {
+    console.log("No Info");
+  } else {
+    var c;
+    Todo.findOne({}, (err, data) => {
+      if (data) {
+        c = data.unique_id + 1;
+      } else {
+        c = 1;
+      }
 
-			if (data) {
-				c = data.unique_id + 1;
-			}else{
-				c =1;
-			}
+      var newtodo = new Todo({
+        unique_id: c,
+        title: todoInfo.todo
+      });
 
-			var newtodo = new Todo({
-				unique_id:c,
-				title: todoInfo.todo
-			});
-
-			newtodo.save(function(err, Person){
-				if(err){
-					console.log(err);
-				}
-				else{
-					console.log(Person);
-				}
-			});
-
-		}).sort({_id: -1}).limit(1);
-	}
-	res.json({Success:'1'});
+      newtodo.save((err, Person) => {
+        if (err) {
+        //   console.log(err);
+        } else {
+        //   console.log(Person);
+        }
+      });
+    })
+      .sort({ _id: -1 })
+      .limit(1);
+  }
+  res.json({ Success: "1" });
 });
 
-
-app.get('/show', function(req, res, next) {
-
-  Todo.find(function(err, response){
+app.get("/show", (req, res, next) => {
+  Todo.find((err, response) => {
     res.json(response);
   });
-
 });
 
-app.delete('/todo/:id', function(req, res) {
-	var id = req.params.id;
+app.delete("/todo/:id", (req, res) => {
+  var id = req.params.id;
 
-	Todo.findOneAndRemove({'unique_id' : id}, function (err,offer){
-		console.log('deleted');
-	});
-	res.send("Success");
-
+  Todo.findOneAndRemove({ unique_id: id }, function(err, offer) {
+    console.log("deleted");
+  });
+  res.send("Success");
 });
 
-app.listen(3000, function(){
-	console.log("Todo in Runing On port 3000");
+app.listen(3000, () => {
+  console.log("Todo in Runing On port 3000");
 });
